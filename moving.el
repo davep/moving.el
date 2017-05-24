d21bdad4fbf521e93b5b10358c8fd2b37d32944d
Dave Pearson <davep@davep.org>
2017-05-24 15:39:51 +0100

    Make the licence clear at the top of the file

--- moving.el
+++ moving.el
@@ -6,6 +6,10 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/moving.el
 
+;; moving.el is free software distributed under the terms of the GNU General
+;; Public Licence, version 2 or (at your option) any later version. For
+;; details see the file COPYING.
+
 ;;; Commentary:
 ;;
 ;; moving.el provides some extra commands for moving around a buffer.
